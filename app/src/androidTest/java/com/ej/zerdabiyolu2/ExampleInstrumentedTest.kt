8c8d0b4dd53145c1bcf01ebbb9fd96da08d04a4b
Farid Adashov <ferid.adasov@gmail.com>
2023-04-08 22:48:51 +0400

    Added  JavaVersion.VERSION_11

--- app/src/androidTest/java/com/ej/zerdabiyolu2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ej/zerdabiyolu2/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.androidtesting", appContext.packageName)
+        assertEquals("com.ej.zerdabiyolu2", appContext.packageName)
     }
 }
